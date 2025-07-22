d922bb522b0df5ca305b22beaaf71ad9b166bad4
hitesh-s0lanki-striq <hitesh.solanki@strique.io>
2025-07-22 19:19:56 +0530

    -- check using sending client ip

--- src/app/api/track-event/route.ts
+++ src/app/api/track-event/route.ts
@@ -1,30 +1,37 @@
-import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const userAgent = req.headers.get("user-agent") || "";
-  const forwardedFor = req.headers.get("x-forwarded-for")?.split(",")[0] || "";
+export async function POST(request: NextRequest) {
+  const xff = request.headers.get("x-forwarded-for");
+  const clientIp = xff?.split(",")[0] || "::1";
 
-  console.log(forwardedFor);
-
-  // build the Plausible payload
-  const payload = {
-    ...body,
-    domain: process.env.PLAUSIBLE_DOMAIN,
+  // put event axios event
+  const plausibleEvent = {
+    domain: "strique.io",
+    name: "test_event_listener",
+    url: `http://localhost:3000/api/test`,
+    props: {
+      test_report: "hitesh.test@strique.io",
+      org_name: "This is from vercel Connect",
+    },
   };
 
-  const resp = await axios.post("https://plausible.io/api/event", payload, {
-    headers: {
-      "User-Agent": userAgent,
-      "X-Forwarded-For": "::1",
-      "Content-Type": "application/json",
-      "X-Debug-Request": "true", // optional, returns what Plausible sees
-    },
-  });
+  try {
+    const response = await axios.post(
+      "https://plausible.io/api/event",
+      plausibleEvent,
+      {
+        headers: {
+          "User-Agent": request.headers.get("User-Agent"), // for accurate visitor deduplication
+          "Content-Type": "application/json",
+          "X-Forwarded-For": clientIp,
+        },
+      }
+    );
 
-  return NextResponse.json(
-    { status: resp.status, data: resp.data },
-    { status: resp.status }
-  );
+    return NextResponse.json({ clientIp, response: response.data });
+  } catch (err) {
+    console.error("❌ Failed to send Plausible event:", err);
+    return NextResponse.json({ message: "Something went wrong" });
+  }
 }
