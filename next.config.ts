cf6149f15edf69a7796150479e1324908a4f86ee
hitesh-s0lanki-striq <hitesh.solanki@strique.io>
2025-07-22 12:50:36 +0530

    -- one more try

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
